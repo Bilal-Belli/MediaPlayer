8082b1db438e5113c1c645018daf4f8708b3a20d
Bilal <jb_belli@esi.dz>
2022-08-02 17:54:42 +0100

    finishing work at mix image with video on player

--- js/script.js
+++ js/script.js
@@ -5,9 +5,34 @@ videoList.forEach(vid =>{
       videoList.forEach(remove =>{remove.classList.remove('active')});
       vid.classList.add('active');
       let src = vid.querySelector('.list-video').src;
-      let title = vid.querySelector('.list-title').innerHTML;
-      document.querySelector('.main-video-container .main-video').src = src;
-      document.querySelector('.main-video-container .main-video').play();
+      let title = vid.querySelector('.list-title').innerHTML; 
+      if (vid.querySelector('.list-video').tagName == "IMG"){
+         document.getElementById('mediaElementBox').remove();
+         const imageRender = document.createElement('img');
+         imageRender.src = src;
+         imageRender.id = 'mediaElementBox';
+         imageRender.controls = true; 
+         imageRender.classList.add('main-video');
+         const box = document.getElementById('box');
+         box.appendChild(imageRender);
+      }
+      else {
+         document.getElementById('mediaElementBox').remove();
+         const videoRender = document.createElement('video');
+         videoRender.src = src;
+         videoRender.id = 'mediaElementBox';
+         videoRender.controls = true; 
+         videoRender.classList.add('main-video');
+         const box = document.getElementById('box');
+         box.appendChild(videoRender);
+      }
       document.querySelector('.main-video-container .main-vid-title').innerHTML = title;
+      document.querySelector('.main-video-container .main-video').play();
    };
 });
+
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+   var x = document.getElementById("myTopnav");
+   if (x.className === "topnav") {x.className += " responsive";} 
+   else {x.className = "topnav";}}
